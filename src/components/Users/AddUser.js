01b41cc7982a55cf90bfc9bce33aff98f335932b
sshilovskiy <N+AJ1o@!_)S>
2022-07-08 15:17:27 +0300

    initial

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,25 @@
+import Card from '../UI/Card';
+import Button from '../UI/Button';
+
+import styles from "./AddUser.module.css"
+
+
+const AddUser = () => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age</label>
+        <input id="age" type="text" />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
